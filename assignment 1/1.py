c731a9d4cc39f1e2eeda935736884c4e0ce73661
Shadow Master <100946203+Quantum5hadow@users.noreply.github.com>
2024-02-12 18:42:29 +0530

    Add files via upload

---
+++ assignment 1/1.py
@@ -0,0 +1,8 @@
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+if num1 > num2:
+    biggest = num1
+else:
+    biggest = num2
+print("The biggest value among", num1, "and", num2, "is:", biggest)
---
+++ assignment 1/3.py
@@ -0,0 +1,26 @@
+student_number = int(input("Enter student number: "))
+student_name = input("Enter student name: ")
+marks_subject1 = float(input("Enter marks for subject 1: "))
+marks_subject2 = float(input("Enter marks for subject 2: "))
+marks_subject3 = float(input("Enter marks for subject 3: "))
+
+total_marks = marks_subject1 + marks_subject2 + marks_subject3
+
+average_marks = total_marks / 3
+
+if average_marks > 75:
+    result = "Distinction"
+elif average_marks > 60:
+    result = "First Class"
+elif average_marks > 50:
+    result = "Second Class"
+elif average_marks > 35:
+    result = "Third Class"
+else:
+    result = "Fail"
+
+print("Student Number:", student_number)
+print("Student Name:", student_name)
+print("Total Marks:", total_marks)
+print("Average Marks:", average_marks)
+print("Result:", result)
